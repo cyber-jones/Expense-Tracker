6356636d3df55d520c208c69067cb4a8821cfc14
Ariyo Jones <jonesariyo00@gmail.com>
2024-10-11 10:40:11 +0100

    cookie commit

--- server/server.js
+++ server/server.js
@@ -46,8 +46,8 @@ app.use(
         saveUninitialized: false, //Save unitialized session
         cookie: {
             maxAge: 1000 * 60 * 60 * 24 * 7,
-            sameSite: "None",
-            secure: true
+            // sameSite: "None",
+            // secure: true
         },
         store,
     })
