5422afccf96319560a273225a1d0d48c32117264
Ariyo Jones <jonesariyo00@gmail.com>
2024-10-11 11:21:43 +0100

    cookie

--- server/server.js
+++ server/server.js
@@ -47,7 +47,7 @@ app.use(
         cookie: {
             maxAge: 1000 * 60 * 60 * 24 * 7,
             httpOnly: true,
-            sameSite: "none",
+            sameSite: 'none',
             secure: true
         },
         store,
