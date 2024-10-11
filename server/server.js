0c3521d8b261a0fe91f813c4b7a3e04c105a5944
Ariyo Jones <jonesariyo00@gmail.com>
2024-10-11 02:05:22 +0100

    cookie commit

--- server/server.js
+++ server/server.js
@@ -37,7 +37,8 @@ app.use(
         cookie: {
             maxAge: 1000 * 60 * 60 * 24 * 7,
             httpOnly: true,
-            sameSite: "none"
+            sameSite: "None",
+            secure: true
         },
         store,
     })
